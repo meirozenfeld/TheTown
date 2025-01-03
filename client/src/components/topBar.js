bc1a30ea77a37208148d9de3fb5ba4681fd652ff
meirozenfeld <57868075+meirozenfeld@users.noreply.github.com>
2025-01-03 22:57:20 +0200

    Update topBar.js

--- client/src/components/topBar.js
+++ client/src/components/topBar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './TopBar.css'; // עיצוב מותאם אישית
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://town-game-client.onrender.com');
 
 const TopBar = ({ role }) => {
   const [isOpen, setIsOpen] = useState(false); // מציין אם החלון פתוח
