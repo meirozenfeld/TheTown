e2cd2ed5725e4b7a356c7abe1e43b1b657062b71
meirozenfeld <57868075+meirozenfeld@users.noreply.github.com>
2025-01-05 12:00:29 +0200

    Update WolvesCounter.js

--- client/src/components/SettingsPage/WolvesCounter.js
+++ client/src/components/SettingsPage/WolvesCounter.js
@@ -6,14 +6,16 @@ const WolvesCounter = ({ wolvesCount, onChange }) => {
   const decrement = () => onChange(Math.max(wolvesCount - 1, 2));  // עדכון הערך לפי הלחיצה
 
   return (
-    <div className="wolves-counter">
-      <h2 className="wolves-title">זאבים</h2>
-      <div className="counter-controls">
-        <button className="counter-button" onClick={decrement}>➖</button>
-        <span className="wolves-number">{wolvesCount}</span>
-        <button className="counter-button" onClick={increment}>➕</button>
-      </div>
-    </div>
+<div className="wolves-counter">
+  <h2 className="feature-title">זאבים</h2>
+  <div className="counter-controls">
+    <button className="counter-button" onClick={decrement}>−</button>
+    <span className="wolves-number">{wolvesCount}</span>
+    <button className="counter-button" onClick={increment}>+</button>
+  </div>
+</div>
+
+
   );
 };
 
