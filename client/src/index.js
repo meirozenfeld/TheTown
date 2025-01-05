72086be31161950f07abdfbd58892b4ff48bf0ff
meirozenfeld <57868075+meirozenfeld@users.noreply.github.com>
2025-01-05 12:02:20 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/styles.css'; // Import the CSS
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
