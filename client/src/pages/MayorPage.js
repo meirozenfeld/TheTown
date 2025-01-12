dd2e4229d617068300dda3be1e651434276de447
meirozenfeld <57868075+meirozenfeld@users.noreply.github.com>
2025-01-12 13:28:35 +0200

    Update MayorPage.js

--- client/src/pages/MayorPage.js
+++ client/src/pages/MayorPage.js
@@ -14,6 +14,53 @@ function MayorPage() {
   const [personalVotes, setPersonalVotes] = useState(false);
   const [role, setRole] = useState('');
   const storedName = sessionStorage.getItem('playerName');
+  const [isLoading, setIsLoading] = useState(true);
+  const [locked, setLocked] = useState(false); // מצב נעילה
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [isFlashing, setIsFlashing] = useState(false);
+
+  useEffect(() => {
+    // בקשה ל-towntime מהשרת
+    socket.emit('requestSettings');
+    socket.on('settingsReceived', (settings) => {
+      console.log('Settings received Vote:', settings);
+      const mayorTimeInSeconds = settings.mayorTime * 60; // המרה לשניות
+      setTimeLeft(mayorTimeInSeconds);
+    });
+
+    return () => {
+      socket.off('settingsReceived');
+    };
+  }, []);
+
+  useEffect(() => {
+    // ספירה לאחור
+    if (timeLeft > 0) {
+      if (timeLeft <= 10) {
+        setIsFlashing(true);
+      } else {
+        setIsFlashing(false);
+      }
+      const timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+      return () => clearTimeout(timer);
+    } else if (!locked && !selectedMayor && players.length > 0) {
+      // בחירה רנדומלית אם הזמן נגמר
+      const currentPlayer = sessionStorage.getItem('playerName'); // שם השחקן הנוכחי
+
+      const filteredPlayersTime = players.filter(player => player.name !== currentPlayer);
+
+      const randomPlayer = filteredPlayersTime[Math.floor(Math.random() * players.length)];
+      setSelectedMayor(randomPlayer.name);
+      setLocked(true);
+    socket.emit('mayorVote', { playerName: currentPlayer, targetName: randomPlayer.name });
+    socket.emit('confirmMayorVote', { 
+      playerName: currentPlayer, 
+      confirm: true // שליחת מצב הנעילה
+    });
+  
+    socket.emit('checkAllMayorConfirmed'); // בדיקה אם כולם אישרו בחירה
+    }
+  }, [timeLeft, locked, selectedMayor, players]);
 
   useEffect(() => {
     socket.emit('requestRolesStructure'); // בקשה לקבלת מבנה התפקידים
@@ -26,6 +73,7 @@ function MayorPage() {
       const currentPlayer = sessionStorage.getItem('playerName'); // שם השחקן הנוכחי
       const filteredPlayers = alivePlayers.filter(p => p.name !== currentPlayer); // סינון השחקן הנוכחי
       setPlayers(filteredPlayers);
+      setIsLoading(false);
     });
   
       // האזנה למבנה התפקידים
@@ -77,15 +125,16 @@ function MayorPage() {
   };
 
   const handleConfirm = () => {
-    if (!selectedMayor) { // אם השחקן לא בחר ראש עיר
-      alert('עליך לבחור ראש עיר לפני לחיצה על "אשר בחירה"!');
+    if (!selectedMayor && !locked) { // אם השחקן לא בחר ראש עיר ולא נעול
+      alert('עליך לבחור ראש עיר לפני לחיצה על "נעל בחירה"!');
       return;
     }
   
-    setConfirmed(!confirmed);
+    const newLockedState = !locked; // שינוי מצב הנעילה
+    setLocked(newLockedState); // עדכון מצב הנעילה
     socket.emit('confirmMayorVote', { 
       playerName: sessionStorage.getItem('playerName'), 
-      confirm: !confirmed 
+      confirm: newLockedState // שליחת מצב הנעילה
     });
   
     socket.emit('checkAllMayorConfirmed'); // בדיקה אם כולם אישרו בחירה
@@ -107,64 +156,88 @@ function MayorPage() {
     socket.emit('checkAllMayorReady');
   };
   
-
+  if (isLoading) {
+    return <h1>טוען נתונים...</h1>;
+  }
   return (
     <div>
-              <TopBar role={role} />
-
-    <h1>שלום {role}!</h1>
+    <TopBar role={role} />
+    <div className={`mayor-page ${mayorName ? 'mayor-selected' : ''}`}>
+     <h1>שלום {role}!</h1>
       {mayorName ? (
-        <div>
+        <div className={`mayor-page ${mayorName ? 'mayor-selected' : ''}`}>
           <h2>ראש העיר הנבחר הוא {mayorName}</h2>
-            <h3>תוצאות ההצבעות:</h3>
-            <ul>
-            {Object.entries(votes).map(([player, count]) => (
-                <li key={player}>
-                {player} קיבל {count} קולות
-                </li>
-            ))}
-            </ul>
-            <h3>בחירות אישיות:</h3>
-            <ul>
-            {Object.entries(personalVotes).map(([voter, target]) => (
-                <li key={voter}>
-                {voter} הצביע ל-{target}
-                </li>
-            ))}
-            </ul>
+          <br></br>
+                  <h2>ספירת קולות:</h2>
+                  <ul className="votes-list">
+                {Object.entries(votes).map(([player, count]) => (
+                  <li key={player}>
+                    <span>{player} קיבל {count} קולות</span>
+                  </li>
+                ))}
+              </ul>
+
+              <h2>בחירות אישיות:</h2>
+              <ul className="votes-list" >
+                  {Object.entries(personalVotes).map(([voter, target]) => (
+                    <li key={voter}>
+                      <span>{voter} הצביע ל-{target}</span>
+                    </li>
+                  ))}
+                </ul>
 
           <button
-            style={{ backgroundColor: isReady ? 'green' : 'red' }}
+            style={{ backgroundColor: isReady ? '#108f14' : '#7e1109' }}
             onClick={handleReady}
           >
-            {isReady ? 'מצב - מוכן' : 'מצב - לא מוכן'}
-          </button>
+            {isReady ? 'במצב - מוכן' : 'במצב - לא מוכן'}
+        </button>
         </div>
       ) : (
         <>
-        <h1>בחר ראש עיר שיקבל 2 קולות בהצבעת העיירה:</h1>
+        <br></br>
+        <h2>
+        כעת העיירה יכולה לדון בבחירות לראש העיר - 
+        אם שחקן לא נבחר בסוף הזמן, הבחירה תהיה רנדומלית 
+        </h2>
+        <h2 className={isFlashing ? 'flashing-timer-mayor' : ''}>
+          הזמן שנותר: {Math.floor(timeLeft / 60)}:{timeLeft % 60}
+        </h2>
+        <h2>בחר ראש עיר שיקבל 2 קולות בהצבעת העיירה:</h2>
+        <br></br>
 
-          {players.map((player) => (
-            <button
-              key={player.name}
-              onClick={() => handleVote(player.name)}
-              style={{
-                backgroundColor: selectedMayor === player.name ? 'blue' : 'red',
-              }}
-            >
-              {player.name}
-            </button>
-          ))}
+        {players.map((player) => (
           <button
-            style={{ backgroundColor: confirmed ? 'green' : 'red' }}
-            onClick={handleConfirm}
+            key={player.name}
+            onClick={() => handleVote(player.name)}
+            disabled={locked} // כפתור יושבת אם במצב נעול
+            style={{
+              backgroundColor: selectedMayor === player.name ? 'darkblue' : '#424442',
+              cursor: locked ? 'not-allowed' : 'pointer', // חץ עכבר משתנה בהתאם למצב הנעילה
+            }}
           >
-            אשר בחירה
+            {player.name}
           </button>
+        ))}
+        <br />
+
+        <button
+          style={{
+            backgroundColor: locked ? '#7e1109' : '#424442', // ירוק - נעול, אדום - פתוח
+            cursor: 'pointer',
+          }}
+          onClick={handleConfirm}
+        >
+          {locked ? 'בחירה נעולה' : 'נעל בחירה'} {/* שינוי טקסט דינמי */}
+        </button>
+
         </>
       )}
     </div>
+</div>
+
   );
+  
 }
 
 export default MayorPage;
